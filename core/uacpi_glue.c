fae7ddfb57b73094662be83cfaa0391532f32eaa
Alon Livne <2005alonlivne@gmail.com>
2024-10-25 20:53:40 +0300

    core: add some uacpi glue implementation

--- core/uacpi_glue.c
+++ core/uacpi_glue.c
@@ -1,21 +1,59 @@
 #include <uacpi/kernel_api.h>
+#include "core/header.h"
+#include "trace.h"
+#include "uacpi/status.h"
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
+extern struct core_header core_header;
+
 uacpi_status uacpi_kernel_get_rsdp(uacpi_phys_addr *out_rdsp_address) {
-  return UACPI_STATUS_UNIMPLEMENTED;
+  TRACE("Returning rsdp...\n");
+  return core_header.rsdp;
 }
 
 uacpi_status uacpi_kernel_raw_memory_read(
     uacpi_phys_addr address, uacpi_u8 byte_width, uacpi_u64 *out_value
 ) {
-  return UACPI_STATUS_UNIMPLEMENTED;
+  switch (byte_width) {
+    case 1:
+      *out_value = *(uint8_t*)address;
+      break;
+    case 2:
+      *out_value = *(uint16_t*)address;
+      break;
+    case 4:
+      *out_value = *(uint32_t*)address;
+      break;
+    case 8:
+      *out_value = *(uint64_t*)address;
+      break;
+    default:
+      return UACPI_STATUS_INVALID_ARGUMENT;
+  }
+  return UACPI_STATUS_OK;
 }
 
 uacpi_status uacpi_kernel_raw_memory_write(
     uacpi_phys_addr address, uacpi_u8 byte_width, uacpi_u64 in_value
 ) {
-  return UACPI_STATUS_UNIMPLEMENTED;
+  switch (byte_width) {
+    case 1:
+      *(uint8_t*)address = in_value;
+      break;
+    case 2:
+      *(uint16_t*)address = in_value;
+      break;
+    case 4:
+      *(uint32_t*)address = in_value;
+      break;
+    case 8:
+      *(uint64_t*)address = in_value;
+      break;
+    default:
+      return UACPI_STATUS_INVALID_ARGUMENT;
+  }
+  return UACPI_STATUS_OK;
 }
 
 uacpi_status uacpi_kernel_raw_io_read(
@@ -47,7 +85,8 @@ uacpi_status uacpi_kernel_pci_write(
 uacpi_status uacpi_kernel_io_map(
     uacpi_io_addr base, uacpi_size len, uacpi_handle *out_handle
 ) {
-  return UACPI_STATUS_UNIMPLEMENTED;
+  *out_handle = (uacpi_handle)(base);
+  return UACPI_STATUS_OK;
 }
 
 void uacpi_kernel_io_unmap(uacpi_handle handle) {
@@ -76,12 +115,33 @@ void uacpi_kernel_unmap(void *addr, uacpi_size len) {
   return;
 }
 
+static char buf[4096 * 16];
+static size_t buf_idx = 0;
+
 void *uacpi_kernel_alloc(uacpi_size size) {
-  return NULL;
+  if (buf_idx + size > sizeof(buf)) {
+    TRACE("Not enough storage for alloc\n");
+    return NULL;
+  }
+
+  void* ptr = &buf[buf_idx];
+  buf_idx += size;
+  return ptr;
 }
 
 void *uacpi_kernel_calloc(uacpi_size count, uacpi_size size) {
-  return NULL;
+  size_t total_size = count * size;
+
+  void* ptr = uacpi_kernel_alloc(total_size);
+  if (ptr == NULL) {
+    return NULL;
+  }
+
+  for (char* i = ptr; i < (char*)ptr + total_size; i++) {
+    *i = 0;
+  }
+
+  return ptr;
 }
 
 void uacpi_kernel_free(void *mem) {
@@ -89,6 +149,9 @@ void uacpi_kernel_free(void *mem) {
 }
 
 void uacpi_kernel_log(uacpi_log_level level, const uacpi_char* log) {
+#ifdef DEBUG
+  trace((char*)log);
+#endif
   return;
 }
 
@@ -104,8 +167,10 @@ void uacpi_kernel_sleep(uacpi_u64 msec) {
   return;
 }
 
+static char mutex;
+
 uacpi_handle uacpi_kernel_create_mutex(void) {
-  return 0;
+  return &mutex;
 }
 
 void uacpi_kernel_free_mutex(uacpi_handle handle) {
@@ -125,7 +190,7 @@ uacpi_thread_id uacpi_kernel_get_thread_id(void) {
 }
 
 uacpi_bool uacpi_kernel_acquire_mutex(uacpi_handle handle, uacpi_u16 val) {
-  return false;
+  return true;
 }
 
 void uacpi_kernel_release_mutex(uacpi_handle handle) {
