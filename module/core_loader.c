54c4cddbecc37481c887ce8003b076a666e95629
Alon Livne <2005alonlivne@gmail.com>
2025-02-09 20:11:38 +0200

    module: remove unneeded exec memory allocation

--- module/core_loader.c
+++ module/core_loader.c
@@ -24,34 +24,14 @@ extern char _binary_build_pm_bin_trimmed_end[];
 #define CORE_PM_START (_binary_build_pm_bin_trimmed_start)
 #define CORE_PM_SIZE ((uintptr_t)_binary_build_pm_bin_trimmed_end - (uintptr_t)_binary_build_pm_bin_trimmed_start)
 
-/*
- * Sets a range of memory to be executable, by turning on the
- * EXEC bit of the page-table entry for all the memory's pages.
+/**
+ * Writes a given buffer to a specified physical address.
+ * @param phys_addr - The physical address to write to.
+ * @param buf       - The buffer to write.
+ * @param size      - The size of the buffer.
+ * @virt_addr_out   - The returned mapped virtual address for the physical address.
  */
-static err_t set_memory_exec(void* start, size_t size) {
-  err_t err = SUCCESS;
-  void* addr;
-  pte_t* addr_pte;
-  unsigned int level;
-
-  addr = start;
-  for (; (uintptr_t)addr < (uintptr_t)start + size; addr += PAGE_SIZE) {
-    addr_pte = lookup_address((uintptr_t)addr, &level);
-    CHECK_TRACE(addr_pte != NULL, "Failed to find the pte of address: %lx", (uintptr_t)addr);
-
-    set_pte(addr_pte, pte_mkexec(*addr_pte));
-  }
-
-cleanup:
-  return err;
-}
-
-/*
- * Allocates executable physical memory [`phys_addr`, `phys_addr` + `size`) and
- * copies `buf` into it. Sets `virt_addr_out` (if not NULL) to the virtual
- * address that maps `phys_addr`.
- */
-static err_t load_exec_phys_memory(uintptr_t phys_addr, void* buf, size_t size, void** virt_addr_out) {
+static err_t load_phys_memory(uintptr_t phys_addr, void* buf, size_t size, void** virt_addr_out) {
   err_t err = SUCCESS;
   void* virt_addr = NULL;
 
@@ -60,9 +40,6 @@ static err_t load_exec_phys_memory(uintptr_t phys_addr, void* buf, size_t size, 
   CHECK_TRACE(virt_addr != NULL, "Failed to map physical addr %lx\n", (uintptr_t)phys_addr);
 
   memcpy(virt_addr, buf, size);
-
-  // TODO: is a cache flush required on x86 after setting memory to exec?
-  CHECK_RETHROW(set_memory_exec(virt_addr, size));
 
   if (virt_addr_out) {
     *virt_addr_out = virt_addr;
@@ -81,11 +58,9 @@ err_t load_core(struct core_header** core_header_out) {
   CHECK_TRACE(CORE_RM_SIZE <= CORE_MAX_RM_PHYS_MEM_SIZE, "Not enough reserved RAM for core rm\n");
   CHECK_TRACE(CORE_PM_SIZE <= CORE_MAX_PM_PHYS_MEM_SIZE, "Not enough reserved RAM for core pm\n");
 
-  // TODO: Why do I care whether this memory is executable? This paging won't be
-  // active when I run....
-  CHECK_RETHROW(load_exec_phys_memory(CORE_PHYS_ADDR, CORE_START, CORE_SIZE, &core_addr));
-  CHECK_RETHROW(load_exec_phys_memory(CORE_RM_PHYS_ADDR, CORE_RM_START, CORE_RM_SIZE, NULL));
-  CHECK_RETHROW(load_exec_phys_memory(CORE_PM_PHYS_ADDR, CORE_PM_START, CORE_PM_SIZE, NULL));
+  CHECK_RETHROW(load_phys_memory(CORE_PHYS_ADDR, CORE_START, CORE_SIZE, &core_addr));
+  CHECK_RETHROW(load_phys_memory(CORE_RM_PHYS_ADDR, CORE_RM_START, CORE_RM_SIZE, NULL));
+  CHECK_RETHROW(load_phys_memory(CORE_PM_PHYS_ADDR, CORE_PM_START, CORE_PM_SIZE, NULL));
 
   core_header = (struct core_header*)core_addr;
   CHECK(is_core_header_magic_valid(core_header));
