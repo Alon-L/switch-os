eba6fe7a82c6d5e2f27657526e95bd682d4520e9
Alon Livne <2005alonlivne@gmail.com>
2025-04-11 13:47:11 +0300

    UEFI: add EFI application to reserve memory
    Core is loaded into a fixed physical address which must not be touched
    by the OS.
    
    The previous implementation passed a `memmap` boot param to the kernel,
    which altered the E820 memory mapping passed to the kernel, in order to
    reserve this memory.
    
    In order to be more generic (and to allow future switch-os
    implementations for more kernels) we do this using a UEFI application.

---
+++ uefi/app.c
@@ -0,0 +1,38 @@
+#include <efi.h>
+#include <efiapi.h>
+#include <efidef.h>
+#include <efierr.h>
+#include <efilib.h>
+
+#include "core/consts.h"
+
+#define PAGE_SIZE (4096)
+
+/**
+ * Allocate pages of type EfiReservedMemoryType to reserve this memory from usage by the OS.
+ */
+static EFI_STATUS reserve_memory(EFI_PHYSICAL_ADDRESS start, size_t size) {
+  size_t pages = size / PAGE_SIZE;
+  return uefi_call_wrapper(BS->AllocatePages, 4, AllocateAddress, EfiReservedMemoryType, pages, &start);
+}
+
+EFI_STATUS
+EFIAPI
+efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
+  InitializeLib(ImageHandle, SystemTable);
+
+  // Reserve memory for core.
+  if (EFI_ERROR(reserve_memory(CORE_PHYS_ADDR, CORE_MAX_PHYS_MEM_SIZE))) {
+    return EFI_LOAD_ERROR;
+  }
+
+  if (EFI_ERROR(reserve_memory(CORE_RM_PHYS_ADDR, CORE_MAX_RM_PHYS_MEM_SIZE))) {
+    return EFI_LOAD_ERROR;
+  }
+
+  if (EFI_ERROR(reserve_memory(CORE_PM_PHYS_ADDR, CORE_MAX_PM_PHYS_MEM_SIZE))) {
+    return EFI_LOAD_ERROR;
+  }
+
+  return EFI_SUCCESS;
+}
