ae79d14a8572d6814bd3f6257a904ede2798607d
Alon Livne <2005alonlivne@gmail.com>
2025-09-30 21:44:55 +0300

    feature: add is_mem_area_contained_in_range utility

--- shared/include/mem_area.h
+++ shared/include/mem_area.h
@@ -1,11 +1,16 @@
 #ifndef _INCLUDE_MEM_AREA_H
 #define _INCLUDE_MEM_AREA_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 struct mem_area {
   uint64_t start;
   uint64_t size;
 };
+
+static inline bool is_mem_area_contained_in_range(const struct mem_area* area, uint64_t start, uint64_t end) {
+  return start <= area->start && area->start + area->size <= end;
+}
 
 #endif
