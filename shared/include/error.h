7a689eab6cff76a5dcb69d6a7f506b80748b085e
Alon Livne <2005alonlivne@gmail.com>
2025-10-01 13:27:50 +0300

    feature: rename ERROR to ERR_INVALID
    It clashes with ERROR in WinAPI, and it doesn't serve a purpose anyway.
    It's basically a non specific error.
    
    In the future, additional error types should be added.

--- shared/include/error.h
+++ shared/include/error.h
@@ -5,7 +5,7 @@
 
 typedef enum {
   SUCCESS = 0,
-  ERROR = 1,
+  ERR_INVALID = 1,
 } err_t;
 
 #define CHECK(expr) CHECK_TRACE((expr), "Error on %s:%u\n", __FILE__, (uint32_t)__LINE__);
@@ -20,30 +20,30 @@ typedef enum {
     }                                                                   \
   } while (0)
 
-#define CHECK_RETHROW_SILENT(expr)                                      \
-  do {                                                                  \
-    err_t _err = (expr);                                                \
-    if (!IS_SUCCESS(_err)) {                                            \
-      err = _err;                                                       \
-      goto cleanup;                                                     \
-    }                                                                   \
+#define CHECK_RETHROW_SILENT(expr) \
+  do {                             \
+    err_t _err = (expr);           \
+    if (!IS_SUCCESS(_err)) {       \
+      err = _err;                  \
+      goto cleanup;                \
+    }                              \
   } while (0)
 
 #define CHECK_TRACE(expr, fmt, ...) \
   do {                              \
     if (!(expr)) {                  \
-      err = ERROR;                  \
+      err = ERR_INVALID;            \
       TRACE(fmt, ##__VA_ARGS__);    \
       goto cleanup;                 \
     }                               \
   } while (0)
 
-#define CHECK_SILENT(expr)          \
-  do {                              \
-    if (!(expr)) {                  \
-      err = ERROR;                  \
-      goto cleanup;                 \
-    }                               \
+#define CHECK_SILENT(expr) \
+  do {                     \
+    if (!(expr)) {         \
+      err = ERR_INVALID;   \
+      goto cleanup;        \
+    }                      \
   } while (0)
 
 #define IS_SUCCESS(expr) ((expr) == SUCCESS)
